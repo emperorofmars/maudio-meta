76b7f9a0c8709f7eca538f5ea78910d831a61d90
Martin Schwarz <martin.schwarz10@gmail.com>
2015-09-09 17:55:05 +0200

    init.sh

--- README.txt
+++
@@ -1,5 +0,0 @@
-
-MAudio Readme:
-
-MAudio is a library for manipulating audio streams with nodes.
-Nodes are loaded as plugins.
---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git clone https://github.com/emperorofmars/libmaudio.git
+git clone https://github.com/emperorofmars/maudio-plugins.git
+git clone https://github.com/emperorofmars/maudio-editor-qt.git
